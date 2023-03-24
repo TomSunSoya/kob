c7ce5700e9df67a1a07b578438eeb809a14e9a55
dqy <qydai1999@gmail.com>
2023-03-24 10:25:30 +0800

    配置MySQL

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,17 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("123456"));
+		System.out.println(encoder.encode("w3dq"));
 	}
 
 }
