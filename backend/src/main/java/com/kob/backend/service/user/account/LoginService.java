803aa7d48f83c62d4843a9f7c928b49b73cf580d
dqy <qydai1999@gmail.com>
2023-03-26 15:23:52 +0800

    backup

--- backend/src/main/java/com/kob/backend/service/user/account/LoginService.java
+++ backend/src/main/java/com/kob/backend/service/user/account/LoginService.java
@@ -3,5 +3,5 @@ package com.kob.backend.service.user.account;
 import java.util.Map;
 
 public interface LoginService {
-    public Map<String, String> login(String username, String password);
+    public Map<String, String> getToken(String username, String password);
 }
