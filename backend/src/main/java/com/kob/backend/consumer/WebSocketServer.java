d894805c0372fea237ea852ed13ca277dc65068a
dqy <qydai1999@gmail.com>
2023-03-30 17:19:47 +0800

    backup

---
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -0,0 +1,72 @@
+package com.kob.backend.consumer;
+
+import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
+public class WebSocketServer {
+    private User user;
+    private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    private Session session = null;
+
+    private static UserMapper userMapper;
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        WebSocketServer.userMapper = userMapper;
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+        // 建立连接
+        this.session = session;
+        int userId = JwtAuthentication.getUserId(token);
+        this.user = userMapper.selectById(userId);
+
+        if (this.user != null)
+            users.put(userId, this);
+        else
+            this.session.close();
+        System.out.println(users);
+    }
+
+    @OnClose
+    public void onClose() {
+        // 关闭链接
+        System.out.println("disconnected!");
+        if (this.user != null)
+            users.remove(this.user.getId());
+
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        // 从Client接收消息
+        System.out.println("receive message");
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    public void sendMessage(String message) {
+        synchronized (this.session) {
+            try {
+                this.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -4,11 +4,39 @@
 
 <script>
 import PlayGround from '../../components/PlayGround';
+import { onMounted, onUnmounted } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
-  components: {
-    PlayGround
-  }
+	components: {
+		PlayGround
+	},
+	setup() {
+		const store = useStore();
+		const socketUrl = `ws://localhost:3000/websocket/${store.state.user.token}/`;
+
+		let socket = null;
+		onMounted(() => {
+			socket = new WebSocket(socketUrl);
+
+			socket.onopen = () => {
+				console.log("connected!");
+			}
+
+			socket.onmessage = msg => {
+				const data = JSON.parse(msg.data);
+				console.log(data);
+			}
+
+			socket.onclose = () => {
+				console.log("disconnected!");
+			}
+		});
+
+		onUnmounted(() => {
+			socket.close();
+		})
+	}
 };
 
 </script>
